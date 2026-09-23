a553d7e80d30fb645965a5aae1813c05e10093f3
ca-pos <c.pos@orange.fr>
2026-09-24 00:59:32 +0200

    working first version

--- constants.py
+++ constants.py
@@ -1,35 +1,42 @@
 # TODO: for development purpose only
-TEMP_DEV = 'tmp'
-#TEMP_DEV = ''
+TEMP_DEV = ''
 
-IMAGES_HOME = '/home/camille/Images/_Importation/PRÉ-TRI/'
-NEF_EXT = 'NEF'
-JPG_EXT = 'JPG'
-EXT_LIST = [NEF_EXT, JPG_EXT]
+IMAGES_HOME = '/home/camille/Images/'
+IMPORT_DIR_ABS = IMAGES_HOME + '_Importation/'
+PRE_SORT_DIR_ABS = IMPORT_DIR_ABS + 'PRÉ-TRI/'
+DEST_DIR_ABS = IMPORT_DIR_ABS + 'ATTENTE_TAGS/' # picture ends up in a "waiting for tags" location
 
-NEF_BASE = 'NEF_BASE'
-JPG_BASE_IPHONE = 'JPG_BASE_IPHONE'
-NONAME_BASE = 'NONAME_BASE'
-PICTURES_BASENAME_LIST = [NEF_BASE, JPG_BASE_IPHONE, NONAME_BASE]
+# NEF_EXT = 'NEF'
+# JPG_EXT = 'JPG'
+# EXT_LIST = [NEF_EXT, JPG_EXT]
 
-REP_OK_MSG = 'Est-ce le bon répertoire O/N (par défaut, O) ? '
-PICTURE_LIST_MSG = '\n\033[0;34m Le répertoire contient les fichiers suivants :\033[00m'
+# NEF_BASE = 'NEF_BASE'
+# JPG_BASE_IPHONE = 'JPG_BASE_IPHONE'
+# NONAME_BASE = 'NONAME_BASE'
+# PICTURES_BASENAME_LIST = [NEF_BASE, JPG_BASE_IPHONE, NONAME_BASE]
+
+# REP_OK_MSG = 'Est-ce le bon répertoire O/N (par défaut, O) ? '
+# PICTURE_LIST_MSG = '\n\033[0;34m Le répertoire contient les fichiers suivants :\033[00m'
 CAT_CHOICE_MSG = 'Choix : '
-EXISTING_CATEGORIES_MSG = 'Catégories disponibles ou en créer une nouvelle'
+EXISTING_CATEGORIES_MSG = 'Catégories disponibles ou en créer une nouvelle ou classement par date'
+EXISTING_SUB_CATEGORIES_MSG = 'Sous catégories disponibles ou en créer une nouvelle'
 CHOICE_ERROR_MSG = 'Ce choix n\'est pas autorisé'
-SELECT_DEST_DIR_MSG = 'Choisir le répertoire de destination'
-CREATE_NEW_DIR_MSG = 'Créer un nouveau répertoire'
+# SELECT_DEST_DIR_MSG = 'Choisir le répertoire de destination'
+# CREATE_NEW_DIR_MSG = 'Créer un nouveau répertoire'
 CREATE_NEW_CAT_MSG = 'Créer une nouvelle catégorie'
-INPUT_MODIFIER_MSG = 'Si nécessaire entrez une lettre d\'ajout à la date compressée (RC si aucune) : '
+CREATE_NEW_SUB_CAT_MSG = 'Créer une nouvelle sous catégorie'
+SORT_BY_DATE_MSG = 'Classer par date'
 PICTURE_DESCRIPTION_MSG = 'Entrez la description de la série d\'images : '
 
 
 INPUT_NEW_CAT_MSG = 'Entrez le nom de la catégorie à créer : '
+INPUT_NEW_SUB_CAT_MSG = 'Entrez le nom de la sous catégorie à créer : '
 CATEGORY_ALREADY_EXISTS_MSG = 'Avertissement. Cette catégorie existe déjà'
 CREATE_CATEGORY_OK_MSG = 'Confirmez-vous la création de la catégorie'
 SELECT_CATEGORY_MESSAGES = {'INPUT_NEW_ITEM_MSG': INPUT_NEW_CAT_MSG,
                             'ITEM_ALREADY_EXISTS_MSG': CATEGORY_ALREADY_EXISTS_MSG,
-                            'CREATE_ITEM_OK_MSG': CREATE_CATEGORY_OK_MSG}
+                            'CREATE_ITEM_OK_MSG': CREATE_CATEGORY_OK_MSG,
+                            'INPUT_NEW_SUB_CAT_MSG': INPUT_NEW_SUB_CAT_MSG}
 
 INPUT_NEW_DIR_MSG = 'Entrez le nom du répertoire à créer : '
 DIRECTORY_ALREADY_EXISTS_MSG = 'Avertissement. Ce répertoire existe déjà'
@@ -38,10 +45,11 @@ SELECT_DIRECTORY_MESSAGES = {'INPUT_NEW_ITEM_MSG': INPUT_NEW_DIR_MSG,
                             'ITEM_ALREADY_EXISTS_MSG': DIRECTORY_ALREADY_EXISTS_MSG,
                             'CREATE_ITEM_OK_MSG': CREATE_DIRECTORY_OK_MSG}
 
-NO_PICTURE_MSG = 'Le répertoire ne contient pas de fichiers images'
+# NO_PICTURE_MSG = 'Le répertoire ne contient pas de fichiers images'
 
-NO_PICTURE_EXIT = 1
+# NO_PICTURE_EXIT = 1
 
-PRE_SORT_DIR_ABS = '/home/camille/Images/_Importation/PRÉ-TRI'
+
+WITH_INFO_FILE = 'with_info.json'
 
 #CATEGORIES = {'1': 'EXERCICES', '2': 'BALADES'}
--- renommage_photos_cli.py
+++ renommage_photos_cli.py
@@ -1,69 +1,16 @@
-import os
+import os, re, json, readline
 from sys import exit
-from pathlib import Path
-from os.path import basename, splitext, abspath
-from prompt_toolkit import prompt
-from prompt_toolkit.completion import PathCompleter
+# from pathlib import Path
+# from os.path import basename, splitext, abspath
+# from prompt_toolkit import prompt
+# from prompt_toolkit.completion import PathCompleter
 from typing import List
-import exifread, string
-
-import re
-import exifread
+# import exifread, string
+# 
+# import exifread
 
 from constants import *
 from set_colors import *
-
-def reject_filter(file: str) -> bool:
-    """ directories to be rejected: 'lost+found' and any hidden ones"""
-    try:
-        file.index('lost+found')
-        return False
-    except ValueError:
-        pass
-    if file.startswith('./.'):
-        return False
-
-    return True
-
-# def select_directory() -> str:
-#     """select directory containing to-be-renamed pictures"""
-#     images_home = IMAGES_HOME+'/2010/6C26'
-#     dir_ok = 'N'
-#     while not dir_ok.upper() == 'O':
-#         os.chdir(images_home)
-#         images_path = prompt(
-#             "Répertoire : " + images_home,
-#             completer=PathCompleter(only_directories=True,file_filter=reject_filter)
-#         )
-#         if images_path:
-#             os.chdir(images_path)
-#         else:
-#             images_path = IMAGES_HOME
-#         print(PICTURE_LIST_MSG)
-#         print('-'*46)
-#         all_files = os.listdir()
-#         all_files.sort()
-#         for file in all_files:
-#             print(file)
-#         print('-'*46)
-#         dir_ok = input(REP_OK_MSG)
-#         print('-'*46)
-#         if dir_ok == '':
-#             dir_ok = 'O'
-#     return images_path
-
-def create_ext_filters() -> dict:
-    """Creates filters for NEF and JP(E)G files. Returns a tuple of filters"""
-    re_nef_ext = re.compile(r".*\.nef$", re.IGNORECASE)     # nef filter
-    re_jpg_ext = re.compile(r".*\.jpe?g$", re.IGNORECASE)   # jpg filter
-    return {NEF_EXT:re_nef_ext, JPG_EXT:re_jpg_ext}
-
-def create_base_filters() -> dict:
-    """Creates filters for original from camera names (base names)"""
-    re_nef_base = re.compile(r".*(DSC_?\d{4}).*\.(NEF|nef)$")                       # Nikon
-    re_jpg_base_iphone = re.compile(r".*(IMG_\d{4}).*\.(JPG|JPEG|jpg|jpeg)$")       # iphone (SE 2020)
-    re_noname_base = re.compile(r".*(XXX-\d{4}).*\.(JPG|JPEG|jpg|jpeg|NEF|nef)$")   # no known name
-    return {NEF_BASE: re_nef_base, JPG_BASE_IPHONE: re_jpg_base_iphone, NONAME_BASE: re_noname_base}
 
 def get_choices(dir: str, upcase: bool) -> dict:
     """Return content of a dir as a dictionnary of choices"""
@@ -99,9 +46,10 @@ def creates_valid_resp_list(length: int, new: List[str]) -> list:
     """
     valid_resp_list = [str(x+1) for x in range(length)]
     valid_resp_list.append(new[0])  #TODO: allows for many additionnal non-numeric responses
+    valid_resp_list.append(new[1])
     return valid_resp_list
 
-def process_response_loop(messages: dict, valid_resp_list: list, upcase:bool, root_path: str, items: dict):
+def process_response_loop(messages: dict, valid_resp_list: list, only_upper:bool, root_path: str, items: dict):
     resp_ok = ''
     while True:
         resp = get_and_check_resp(valid_resp_list)
@@ -109,11 +57,12 @@ def process_response_loop(messages: dict, valid_resp_list: list, upcase:bool, ro
             continue
         elif resp.upper() == 'N':
             while not resp_ok.upper() == 'O':
-                msg = messages['INPUT_NEW_ITEM_MSG']
+                msg = 'INPUT_NEW_ITEM_MSG' if only_upper else 'INPUT_NEW_SUB_CAT_MSG' 
+                msg = messages[msg]
                 new_item = input(set_blue(msg))
-                if upcase:
+                if only_upper:
                     new_item = new_item.upper()
-                path_to_new_item = root_path+'/'+new_item
+                path_to_new_item = root_path+new_item
                 if new_item in items.values():
                     msg = SELECT_DIRECTORY_MESSAGES['ITEM_ALREADY_EXISTS_MSG']
                     print(set_yellow(msg))
@@ -127,66 +76,110 @@ def process_response_loop(messages: dict, valid_resp_list: list, upcase:bool, ro
             except FileExistsError:
                 print('FileExistsError')
             return path_to_new_item
+        elif resp.upper() == 'D':
+            return '@'
         else:
-            path_to_item = root_path+'/'+items[resp]
-            print('CHOISIR '+path_to_item)
+            path_to_item = root_path+items[resp]
             return path_to_item
 
-def select_category() -> str:
-    """Select category where to save pictures"""
-    print(set_blue(EXISTING_CATEGORIES_MSG))
-    categories = get_choices(IMAGES_HOME, True)
-    valid_resp_list = creates_valid_resp_list(len(categories), ['N'])
-    categories['N'] = CREATE_NEW_CAT_MSG    # add possibility to create a new category
+def select_category(dir_:str, only_upper:bool) -> str:
+    """Select category where to save pictures if only_upper (by convention, categories are upper case)
+        Select subcategory where to save pictures if not only_upper
+    """
+    msg = EXISTING_CATEGORIES_MSG if only_upper else EXISTING_SUB_CATEGORIES_MSG
+    print(set_blue(msg))
+    categories = get_choices(dir_, only_upper)
+    valid_resp_list = creates_valid_resp_list(len(categories), ['N', 'D'])
+    if only_upper:
+        categories['D'] = SORT_BY_DATE_MSG
+    categories['N'] = CREATE_NEW_CAT_MSG if only_upper else CREATE_NEW_SUB_CAT_MSG
     display_choices(categories)
-    path_to_category = process_response_loop(SELECT_CATEGORY_MESSAGES, valid_resp_list, True, IMAGES_HOME+TEMP_DEV, categories)
-    return path_to_category
+    path_ = process_response_loop(SELECT_CATEGORY_MESSAGES, valid_resp_list, only_upper, dir_ , categories)
+    return path_+'/'
 
-def select_dest_dir():
-    """Returns rep (within the proper category) to store renamed pictures"""
-    path_to_category = select_category()
-    choices = get_choices(path_to_category, False)
-    valid_resp_list = creates_valid_resp_list(len(choices), ['N'])
-    choices['N'] = CREATE_NEW_DIR_MSG
-    print(set_blue(SELECT_DEST_DIR_MSG))
-    display_choices(choices)
-    dest_dir = process_response_loop(SELECT_DIRECTORY_MESSAGES, valid_resp_list, False, path_to_category, choices)
-    return dest_dir
+def unused():
+# def select_dest_dir():
+#     """Returns rep (within the proper category) to store renamed pictures"""
+#     path_to_category = select_category()
+#     choices = get_choices(path_to_category, False)
+#     valid_resp_list = creates_valid_resp_list(len(choices), ['N'])
+#     choices['N'] = CREATE_NEW_DIR_MSG
+#     print(set_blue(SELECT_DEST_DIR_MSG))
+#     display_choices(choices)
+#     dest_dir = process_response_loop(SELECT_DIRECTORY_MESSAGES, valid_resp_list, False, path_to_category, choices)
+#     return dest_dir
 
-def create_date_part_from_nef(file:str, modifier:str)->str:
-        months_as_letter = string.ascii_uppercase[0:12]
-        with open(file, 'rb') as img_file:
-            tags = exifread.process_file(img_file)
-            exif_date = str(tags['EXIF DateTimeOriginal'])
-            year = exif_date[0:4]
-            month = exif_date[5:7]
-            day = exif_date[8:10]
-            date = '-'.join([year, month, day])
-            i_month = int(month)
-            month_as_letter = months_as_letter[i_month-1:i_month]
-            compressed_date = year[-1]+month_as_letter+day
-            date_part = date+'_('+compressed_date+modifier+')'
-            return date_part
+# def create_date_part_from_nef(file:str, modifier:str)->str:
+#         months_as_letter = string.ascii_uppercase[0:12]
+#         with open(file, 'rb') as img_file:
+#             tags = exifread.process_file(img_file)
+#             exif_date = str(tags['EXIF DateTimeOriginal'])
+#             year = exif_date[0:4]
+#             month = exif_date[5:7]
+#             day = exif_date[8:10]
+#             date = '-'.join([year, month, day])
+#             i_month = int(month)
+#             month_as_letter = months_as_letter[i_month-1:i_month]
+#             compressed_date = year[-1]+month_as_letter+day
+#             date_part = date+'_('+compressed_date+modifier+')'
+#             return date_part
+#
+# def process_nef(file:str, base:str, modifier: str, file_num: int, description:str):
+#     """Rename NEF (Nikon) picture file including .xmp file if present"""
+#     # (2019-12-13)_001__DSC8376-9L13_neige_gache_et_terrasse_est.NEF
+#     abbrev_month = [x for x in range(12)]
+#     if not '_' in base:
+#         base = '_'+base
+#     os.chdir('/home/camille/Images/tmp_nef_map')
+#     date_part = create_date_part_from_nef(file, modifier)
+#     tmp, ext = os.path.splitext(file)
+#   
+#     new_name = date_part + '_' + f"{file_num:03}" + '_[' + base +']_' + description + ext
+#     print('nwnwnw', new_name)
+#   
+# def process_jpg_iphone(file, base, ext):
+#     """Rename JPEG picture file taken with an iphone (presently, SE 2020 model)"""
+#     pass
+# def process_noname(file, base, ext):
+#     """Rename picture taken with an unknown camera"""
+#     pass
+# def create_ext_filters() -> dict:
+#     """Creates filters for NEF and JP(E)G files. Returns a tuple of filters"""
+#     re_nef_ext = re.compile(r".*\.nef$", re.IGNORECASE)     # nef filter
+#     re_jpg_ext = re.compile(r".*\.jpe?g$", re.IGNORECASE)   # jpg filter
+#     return {NEF_EXT:re_nef_ext, JPG_EXT:re_jpg_ext}
+# def reject_filter(file: str) -> bool:
+#     """ directories to be rejected: 'lost+found' and any hidden ones"""
+#     try:
+#         file.index('lost+found')
+#         return False
+#     except ValueError:
+#         pass
+#     if file.startswith('./.'):
+#         return False
 
-def process_nef(file:str, base:str, modifier: str, file_num: int, description:str):
-    """Rename NEF (Nikon) picture file including .xmp file if present"""
-    # (2019-12-13)_001__DSC8376-9L13_neige_gache_et_terrasse_est.NEF
-    abbrev_month = [x for x in range(12)]
-    if not '_' in base:
-        base = '_'+base
-    os.chdir('/home/camille/Images/tmp_nef_map')
-    date_part = create_date_part_from_nef(file, modifier)
-    tmp, ext = os.path.splitext(file)
-    
-    new_name = date_part + '_' + f"{file_num:03}" + '_[' + base +']_' + description + ext
-    print('nwnwnw', new_name)
-    
-def process_jpg_iphone(file, base, ext):
-    """Rename JPEG picture file taken with an iphone (presently, SE 2020 model)"""
-    pass
+#     return True
+# def create_base_filters() -> dict:
+#     """Creates filters for original from camera names (base names)"""
+#     re_nef_base = re.compile(r".*(DSC_?\d{4}).*\.(NEF|nef)$")                       # Nikon
+#     re_jpg_base_iphone = re.compile(r".*(IMG_\d{4}).*\.(JPG|JPEG|jpg|jpeg)$")       # iphone (SE 2020)
+#     re_noname_base = re.compile(r".*(XXX-\d{4}).*\.(JPG|JPEG|jpg|jpeg|NEF|nef)$")   # no known name
+#     return {NEF_BASE: re_nef_base, JPG_BASE_IPHONE: re_jpg_base_iphone, NONAME_BASE: re_noname_base}
 
-def process_noname(file, base, ext):
-    """Rename picture taken with an unknown camera"""
+# def enter_type():
+#         print("1. NEF")
+#         print("2. JPG/JPEG")
+#         f_type = " "
+#         while f_type not in ["", "1", "2"]:
+#             f_type = input("Type de fichiers [par défaut, 1] : ")
+#             if f_type == "":
+#                 f_type = "1"
+
+#         return int(f_type)-1
+
+# def enter_modifier():
+#     return input('Lettre à ajouter (entrée si aucune) :')
+
     pass
 
 def get_description() -> str:
@@ -198,47 +191,86 @@ def get_description() -> str:
     return description
 
 def walk_through():
+    dirs_to_visit = list()
     walk_return_lst = list(os.walk(os.getcwd()))
     for f in walk_return_lst:
         if f[2]:
-            print('...', f[2])
+            dirs_to_visit.append(f[0])
+    dirs_to_visit.sort()
+    del dirs_to_visit[0]    # 1st dir contains with_info dict, no pictures
+    return dirs_to_visit
+        
 
-
-def do_processing(filters: dict):
+def do_processing():
     """Figure out which types of pictures are presents"""
-    processes = {NEF_BASE: process_nef, JPG_BASE_IPHONE: process_jpg_iphone, NONAME_BASE: process_noname}
-    walk_through()
-    exit()
-    all_files = os.listdir('.')
-    all_files.sort()
-    modifier = input(set_blue(INPUT_MODIFIER_MSG))
-    flag = False
-    file_num = 1
-    description = get_description()
-    for file in all_files:
-        for index in range(len(filters)):
-            temp = filters[PICTURES_BASENAME_LIST[index]].findall(file)
-            if bool(temp):
-                if not flag:
-                    dest_rep = select_dest_dir()
-                    os.chdir(dest_rep)
-                    flag = True
-                base, ext = temp[0]
-                processes[PICTURES_BASENAME_LIST[index]](file, base, modifier, file_num, description)
-        file_num +=1
-    if not flag:
-        print(set_red(NO_PICTURE_MSG))
-        exit(NO_PICTURE_EXIT)
+    # processes = {NEF_BASE: process_nef, JPG_BASE_IPHONE: process_jpg_iphone, NONAME_BASE: process_noname}
+    with_info = get_with_info_file()    # created by renommage_photos_ui (from PictureWithInfo object)
+    dirs_to_visit = walk_through()
+    for dir_index in range(len(dirs_to_visit)):
+        print(dirs_to_visit[dir_index])
+        os.chdir(dirs_to_visit[dir_index])
+        dir_content = os.listdir()
+        print('dircont', dir_content)
+        dir_content.sort()
+        file_info = get_fileinfo(with_info, dir_content)
+        description = get_description()
+        new_names = get_newnames(file_info, len(dir_content), description)
+        path_to_category = select_category(DEST_DIR_ABS, True)
+        if path_to_category == "@/":
+            print('CLASSER PAR DATE')
+        else:
+            path_to_subcategory = select_category(path_to_category, False)
+            for new_name in new_names:
+                file_to_move = file_info[0][5]
+                to_go = path_to_subcategory+new_name
+                print('...', file_to_move, to_go)
+        print('-'*70)
 
-    return
+def get_newnames(file_info, length, description):
+        new_names = list()
+        for index in range(length):
+            file_root = file_info[index]
+            new_name = file_root[0]+'('+file_root[1]+')_['+file_root[2]+']-'+description
+            counter = file_root[3]
+            num_part = f'_{counter:03d}'
+            ext_ = file_root[4]            
+            new_name = new_name + ('' if length == 1 else num_part) + ext_
+            new_names.append(new_name)
+        return new_names
+
+
+
+def get_with_info_file():
+    with_info = dict()
+    with open(PRE_SORT_DIR_ABS+WITH_INFO_FILE, 'r') as file:
+        with_info = json.load(file)
+    return with_info
+
+def get_fileinfo(with_info, dir_content):
+    file_info = list()
+    counter = 0
+    for file in dir_content:
+        print('    Fichier : ', file)
+        counter += 1
+        base, ext_ = os.path.splitext(file)
+        date = with_info[base][1]
+        compressed_date = with_info[base][3]
+        print('Commentaire : ', with_info[base][0])
+        num_part = f'_{counter:03d}'
+        # file_info = info needed in the following
+        # 0:date, 
+        # 1:compressed date, 
+        # 2:filename (no extension), 
+        # 3:count of pictures in the dir,
+        # 4:extention
+        # 5:filename (full)
+        file_info.append((date, compressed_date, base, counter, ext_, file))
+    return file_info
+
 
 def rename_pictures():
-    """
-    possible extensions are NEF and JPG/JPEG
-    """
     os.chdir(PRE_SORT_DIR_ABS)
-    base_filters = create_base_filters()
-    do_processing(base_filters)
+    do_processing()
     return
 
     # 5. move picture files
@@ -290,21 +322,6 @@ def format_description(string: str) -> str:
         string = string.replace('\'', '_')  # replace apostrophes whith underlines
     return string
 
-# def enter_type():
-#         print("1. NEF")
-#         print("2. JPG/JPEG")
-#         f_type = " "
-#         while f_type not in ["", "1", "2"]:
-#             f_type = input("Type de fichiers [par défaut, 1] : ")
-#             if f_type == "":
-#                 f_type = "1"
-
-#         return int(f_type)-1
-
-# def enter_modifier():
-#     return input('Lettre à ajouter (entrée si aucune) :')
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
         # process_nef('(2019-12-13)_001__DSC8376-9L13_neige_gache_et_terrasse_est.NEF','_DSC8376','NEF')
         # exit()
